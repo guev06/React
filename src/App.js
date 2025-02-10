b611179059a2122bd0805a59a27a019580037d9f
guev06 <jojamisto@gmail.com>
2025-02-10 16:11:25 +0200

    add UserList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function UserList(props){
+  console.log(props.UserList)
+  return <ul>
+    {props.UserList.map((item,index)=>{
+      return <li key={index}>{item.id} {item.name} {item.email}</li>
+})}
+  </ul>
+}
 function App() {
+  const myList =[{id:1, name:"name", email:"lala@gmail.com"}]
+
   return (
     <div className="App">
-      new
+      <UserList UserList={myList}></UserList>
     </div>
+    
   );
 }
 
